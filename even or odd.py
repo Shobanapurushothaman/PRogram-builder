079f28e43d3fb3d684c056ec7577716e143a8da5
Shobanapurushothaman <34570269+Shobanapurushothaman@users.noreply.github.com>
2017-12-16 14:17:16 +0530

    Add files via upload

---
+++ even or odd.py
@@ -0,0 +1,13 @@
+a=int(raw_input())
+
+if(a==0):
+	
+         print("The given number is neither even nor odd")
+
+elif(a%2==0):
+	
+         print("The given number is even")
+
+else:
+	
+         print("The given number is odd")
