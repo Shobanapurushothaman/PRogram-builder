eca98bb02d0cb9589a385568bd4a947ddaa3a3ab
Shobanapurushothaman <34570269+Shobanapurushothaman@users.noreply.github.com>
2017-12-16 15:22:46 +0530

    Add files via upload

---
+++ greatest among 3.py
@@ -0,0 +1,19 @@
+print(" Enter three numbers")
+
+a=int(raw_input())
+
+b=int(raw_input())
+
+c=int(raw_input())
+
+if(a>b and a>c ):
+	
+            print("%d is the greatest."%a)
+
+elif(b>c and b>a):
+	
+           print("%d is the greatest."%b)
+
+else:
+            
+           print("%d is the greatest."%c)
