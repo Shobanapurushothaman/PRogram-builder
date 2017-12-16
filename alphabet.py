ba8300fe7fed0d3fcdc79d2016bc42080035c543
Shobanapurushothaman <34570269+Shobanapurushothaman@users.noreply.github.com>
2017-12-16 14:58:08 +0530

    Add files via upload

---
+++ alphabet.py
@@ -0,0 +1,11 @@
+print("Enter the character")
+
+x=raw_input()
+
+if(x>='a' and x<='z'):
+	
+                  print("%c is an alphabet."%x)
+
+else:
+	
+                  print("%c is not an alphabet")
